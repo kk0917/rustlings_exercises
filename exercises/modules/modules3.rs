24c4f12bcf73e6618fd9b18d53513d109b61dffa
kk0917 <kamenash0917@gmail.com>
2024-03-06 11:59:32 +0900

    done modules3

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -8,10 +8,10 @@
 // I AM NOT DONE
 
 // TODO: Complete this use statement
-use ???
+use std::time as time;
 
 fn main() {
-    match SystemTime::now().duration_since(UNIX_EPOCH) {
+    match time::SystemTime::now().duration_since(time::UNIX_EPOCH) {
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
